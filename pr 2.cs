9d6885051ed5242a381dda9f74f504489d732a07
anton2121212 <122678473+anton2121212@users.noreply.github.com>
2023-03-16 23:59:01 +0300

    Add files via upload

---
+++ pr 2.cs
@@ -0,0 +1,150 @@
+﻿// See https://aka.ms/new-console-template for more information
+//задание 1
+//int[] numbers = new int[] { 5, 2, 8, 1, 3, 9 }; 
+//int firstNumber = numbers[0]; 
+
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] == firstNumber) 
+//    {
+//        Console.WriteLine($"Элемент {firstNumber} найден на позиции {i}");
+//        break; 
+//    }
+//}
+
+//задание 2
+
+
+//int[] arr = { 10, -20, 30, -40, 50, 60, -70, 80, 90, 100 };
+
+//// а) все неотрицательные элементы
+//Console.WriteLine("Неотрицательные элементы массива:");
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] >= 0)
+//    {
+//        Console.Write(arr[i] + " ");
+//    }
+//}
+//Console.WriteLine();
+
+//// б) все элементы, не превышающие число 100
+//Console.WriteLine("Элементы массива, не превышающие 100:");
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] <= 100)
+//    {
+//        Console.Write(arr[i] + " ");
+//    }
+//}
+//Console.WriteLine();
+
+//// в) все четные элементы
+//Console.WriteLine("Четные элементы массива:");
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] % 2 == 0)
+//    {
+//        Console.Write(arr[i] + " ");
+//    }
+//}
+//Console.WriteLine();
+
+
+//задание 3
+
+
+//int[] arr = { 5, -3, 4, -8, -9, 1, 9, 6, -3, 4, 8, 2 };
+//int negativeCount = 0;
+
+//foreach (int num in arr)
+//{
+//    if (num < 0)
+//    {
+//        negativeCount++;
+//    }
+//}
+
+//Console.WriteLine($"Количество отрицательных элементов в массиве: {negativeCount}");
+
+
+//задание 4
+
+//int[] arr = { 5, 3, 8, 2, 9, 7, 6, 1, 4 };
+//double sum = 0;
+//int count = 0;
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (i % 2 != 0) 
+//    {
+//        sum += arr[i]; 
+//        count++; 
+//    }
+//}
+
+//double avg = sum / count;
+
+//Console.WriteLine($"Среднее арифметическое элементов с нечетными номерами: {avg}");
+
+//задание 5
+
+
+        //int[] arr = { 3, 7, 2, 9, 5 };
+
+        //int firstElement = arr[0]; 
+
+        //for (int i = 0; i < arr.Length; i++)
+        //{
+        //    arr[i] += firstElement; 
+        //}
+
+        //Console.WriteLine($"Новые значения элементов массива: {string.Join(", ", arr)}");
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
